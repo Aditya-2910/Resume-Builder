3a35b1d58146c2bd012c73d4beb30a8eac2b1c40
Aditya <asaditya2001@gmail.com>
2022-12-12 10:36:41 +0530

    Added Login.js file

---
+++ src/components/login.js
@@ -0,0 +1,17 @@
+import {useForm} from 'react-hook-form';
+import { useState } from 'react';
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
+
+
+function login() {
+  return (
+    <div>
+        <form></form>
+
+
+    </div>
+  )
+}
+
+export default login
